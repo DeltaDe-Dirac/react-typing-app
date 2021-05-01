59880e324494b55ce471b65140e31acbb5a3a983
Delta de Dirac <igorshevtsov@gmail.com>
2021-05-01 20:15:17 +0300

    install and initialize the app with firebase

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import FirebaseProvider from "./utils/firebase";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <FirebaseProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </FirebaseProvider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
