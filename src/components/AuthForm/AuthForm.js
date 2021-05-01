5c5858187fe9b8a8ba472dba82a7fd115234c4b0
Delta de Dirac <igorshevtsov@gmail.com>
2021-05-01 15:45:20 +0300

    add auth Sing Up variant

--- src/components/AuthForm/AuthForm.js
+++ src/components/AuthForm/AuthForm.js
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./AuthForm.css";
 import { Form, Col, Button } from "react-bootstrap";
 
-export default function LoginForm() {
+export default function LoginForm({ isLogin }) {
   const [validated, setValidated] = useState(false);
 
   const handleSubmit = (event) => {
@@ -15,21 +15,43 @@ export default function LoginForm() {
     setValidated(true);
   };
 
+  useEffect(() => {
+    return () => {
+      setValidated(false);
+    };
+  }, [isLogin]);
+
   return (
     <Form noValidate validated={validated} onSubmit={handleSubmit} className="c-loginForm">
+      {!isLogin ? (
+        <>
+          <Form.Row>
+            <Form.Group as={Col} md="8" controlId="validationCustom01">
+              <Form.Control required type="text" placeholder="First Name" maxLength={25} />
+              <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+          <Form.Row>
+            <Form.Group as={Col} md="8" controlId="validationCustom02">
+              <Form.Control required type="text" placeholder="Last Name" maxLength={25} />
+              <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Row>
+        </>
+      ) : null}
       <Form.Row>
-        <Form.Group as={Col} md="8" controlId="validationCustom01">
+        <Form.Group as={Col} md="8" controlId="validationCustom03">
           <Form.Control required type="text" placeholder="Username or Email" maxLength={25} />
           <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>
         </Form.Group>
       </Form.Row>
       <Form.Row className="submitForm">
-        <Form.Group as={Col} md="8" controlId="validationCustom02">
+        <Form.Group as={Col} md="8" controlId="validationCustom04">
           <Form.Control required type="password" placeholder="Password" />
           <Form.Control.Feedback type="invalid">Required Field</Form.Control.Feedback>
         </Form.Group>
         <Button type="submit" variant="outline-success">
-          Sign In
+          {isLogin ? "Sign In" : "Sing Up"}
         </Button>
       </Form.Row>
     </Form>
--- src/components/AuthModal/AuthModal.js
+++ src/components/AuthModal/AuthModal.js
@@ -5,19 +5,21 @@ import AuthForm from "../AuthForm/AuthForm";
 import { Link } from "react-router-dom";
 
 export default function AuthModal({ show, setHide }) {
+  const [isLogin, setIsLogin] = useState(true);
+
   return (
     <>
       <Modal show={show} onHide={setHide} backdrop="static" keyboard={false} className="c-authModal">
         <Modal.Header closeButton>
           <Modal.Title>
-            Log In or{" "}
-            <Link to="#" onClick={() => alert(" Sing Up")}>
-              Sing Up
+            {isLogin ? "Log In or " : "Sign Up or "}
+            <Link to="#" onClick={() => setIsLogin(!isLogin)}>
+              {isLogin ? "Sign Up" : "Log In"}
             </Link>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <AuthForm />
+          <AuthForm isLogin={isLogin} />
         </Modal.Body>
         <Modal.Footer>
           <Link to="#" onClick={() => alert("Sign in with Google")}>
