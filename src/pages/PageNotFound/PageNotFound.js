66bde67aaffb7068dab0e52384b2f971eabe6bea
Delta de Dirac <igorshevtsov@gmail.com>
2021-05-02 20:11:08 +0300

    add go back link to page not found

--- src/pages/PageNotFound/PageNotFound.js
+++ src/pages/PageNotFound/PageNotFound.js
@@ -1,15 +1,20 @@
 import React, { useState } from "react";
 import { Redirect } from "react-router";
+import { useHistory } from "react-router-dom";
+
 import { Nav } from "react-bootstrap/";
 
 import "./PageNotFound.css";
 import { Container } from "react-bootstrap";
 
 export default function PageNotFound({ imgPath, errCode, errText }) {
+  let history = useHistory();
   const [redirectTo, setRedirectTo] = useState(null);
 
-  if (redirectTo !== null) {
-    return <Redirect to={redirectTo}></Redirect>;
+  if (redirectTo === "home") {
+    return <Redirect to="/"></Redirect>;
+  } else if (redirectTo === "back") {
+    return <Redirect to={history.goBack()}></Redirect>;
   }
 
   const img = process.env.PUBLIC_URL.concat(imgPath);
@@ -20,8 +25,11 @@ export default function PageNotFound({ imgPath, errCode, errText }) {
         <h1>{errCode}</h1>
         <div className="notfound">
           <p className="text-uppercase">{errText}</p>
-          <Nav.Link eventKey="/" onSelect={(selectedKey) => setRedirectTo(selectedKey)}>
+          <Nav.Link eventKey="home" onSelect={(selectedKey) => setRedirectTo(selectedKey)}>
             Home
+          </Nav.Link>
+          <Nav.Link eventKey="back" onSelect={(selectedKey) => setRedirectTo(selectedKey)}>
+            Back
           </Nav.Link>
         </div>
       </Container>
