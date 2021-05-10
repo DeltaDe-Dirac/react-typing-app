0e8d90c2f23d9f0c4103a35a00b419adbe587497
Delta de Dirac <igorshevtsov@gmail.com>
2021-05-10 16:06:22 +0300

    handle nav menu show-hide + design and layout

--- src/components/TypeMeNavBar/TypeMeNavBar.js
+++ src/components/TypeMeNavBar/TypeMeNavBar.js
@@ -1,55 +1,99 @@
+import { useState } from "react";
 import "./TypeMeNavBar.css";
 import { Nav, Navbar } from "react-bootstrap";
 import { useHistory } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-export default function TypeMeNavBar() {
+export default function TypeMeNavBar({ hideMe, resetHideMe }) {
   const hist = useHistory();
+  const [floatMenu, toggleFloatMenu] = useState(null);
+
+  const handleSelect = (eventKey) => {
+    floatMenu && eventKey === floatMenu && !hideMe ? toggleFloatMenu(null) : toggleFloatMenu(eventKey);
+    resetHideMe();
+  };
+
+  const handleHide = (e) => {
+    if (e.type === "click") {
+      toggleFloatMenu(null);
+      resetHideMe();
+    }
+  };
+
   return (
     <div className="c-typemeNavBarWrap">
       <Navbar bg="dark" variant="dark">
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
-        <Navbar.Collapse id="responsive-navbar-nav">
+        <Navbar.Collapse id="responsive-navbar-nav" onClick={(e) => handleHide(e)}>
           <Nav>
             <Nav.Link href="#" onClick={() => hist.goBack()}>
-              <FontAwesomeIcon className="fa-icon" icon="arrow-alt-circle-left" />
+              <FontAwesomeIcon icon={["fa", "arrow-alt-circle-left"]} />
             </Nav.Link>
 
             <Navbar.Brand>Lesson 223: Human Body</Navbar.Brand>
           </Nav>
         </Navbar.Collapse>
-        <Nav variant="tabs" defaultActiveKey="/home">
+        <Nav variant="tabs" defaultActiveKey="/home" onSelect={(eventKey) => handleSelect(eventKey)}>
           <Nav.Item>
-            <Nav.Link href="#">Active</Nav.Link>
+            <Nav.Link href="#" eventKey="restart" active={false}>
+              <FontAwesomeIcon icon={["fas", "undo"]} />
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="link-1">Option 2</Nav.Link>
+            <Nav.Link href="#" eventKey="sound" active={floatMenu === "sound" && !hideMe}>
+              <FontAwesomeIcon icon={["fas", "volume-up"]} />
+            </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link eventKey="disabled">Disabled</Nav.Link>
+            <Nav.Link href="#" eventKey="settings" active={floatMenu === "settings" && !hideMe}>
+              <FontAwesomeIcon icon={["fa", "cog"]} />
+            </Nav.Link>
           </Nav.Item>
         </Nav>
       </Navbar>
-      <Navbar expand bg="dark" variant="dark" className="floatMenu">
-        <Nav className="justify-content-end" activeKey="/home">
-          <Nav.Link href="/home">Active</Nav.Link>
-
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
-        </Nav>
+      <Navbar
+        expand
+        bg="dark"
+        variant="dark"
+        className={floatMenu === "settings" && !hideMe ? `floatMenu ${floatMenu}` : "floatMenu"}
+      >
         <Nav defaultActiveKey="/home" className="flex-column">
-          <Nav.Link href="/home">Active</Nav.Link>
-          <Nav.Link eventKey="link-1">Link</Nav.Link>
-          <Nav.Link eventKey="link-2">Link</Nav.Link>
-          <Nav.Link eventKey="disabled" disabled>
-            Disabled
-          </Nav.Link>
+          <div className="form-check form-switch">
+            <label className="form-check-label" htmlFor="stats">
+              Show WPM and Accuracy
+            </label>
+            <input className="form-check-input" type="checkbox" id="stats"></input>
+          </div>
+          <hr />
+          <div className="form-check form-switch">
+            <label className="form-check-label" htmlFor="errors">
+              Block on Error(s)
+            </label>
+            <input className="form-check-input" type="checkbox" id="errors"></input>
+          </div>
+        </Nav>
+      </Navbar>
+      <Navbar
+        expand
+        bg="dark"
+        variant="dark"
+        className={floatMenu === "sound" && !hideMe ? `floatMenu ${floatMenu}` : "floatMenu"}
+      >
+        <Nav defaultActiveKey="/home" className="flex-column">
+          <div className="form-check form-switch">
+            <label className="form-check-label" htmlFor="keyboard">
+              Keyboard Sound
+            </label>
+            <input className="form-check-input" type="checkbox" id="keyboard"></input>
+          </div>
+          <hr />
+          <div className="form-check form-switch">
+            <label className="form-check-label" htmlFor="voice">
+              Voice Over
+            </label>
+            <input className="form-check-input" type="checkbox" id="voice"></input>
+          </div>
         </Nav>
       </Navbar>
     </div>
--- src/pages/TypingPage/TypingPage.js
+++ src/pages/TypingPage/TypingPage.js
@@ -1,12 +1,27 @@
-import { Container } from "react-bootstrap";
+import { useState } from "react";
 import "./TypingPage.css";
 
+import { Container } from "react-bootstrap";
 import TypeMeNavBar from "../../components/TypeMeNavBar/TypeMeNavBar";
 
 export default function TypingPage({ typeMe }) {
+  const nodeNames = ["path", "A", "svg", "INPUT", "LABEL", "HR"];
+  const [hideMe, setHideMe] = useState(false);
+
   return (
-    <div className="p-typingPageWrap">
-      <TypeMeNavBar />
+    <div
+      className="p-typingPageWrap"
+      onClick={(e) => {
+        if (
+          !nodeNames.includes(e.target.nodeName) &&
+          !(e.target.nodeName === "NAV" && e.target.classList.contains("floatMenu")) &&
+          !(e.target.nodeName === "DIV" && e.target.classList.contains("navbar-nav"))
+        ) {
+          setHideMe(true);
+        }
+      }}
+    >
+      <TypeMeNavBar hideMe={hideMe} resetHideMe={() => setHideMe(false)} />
 
       <Container>
         <h3>{typeMe}</h3>
